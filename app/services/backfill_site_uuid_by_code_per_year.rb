9dfd1a288e5766383d8e7b99332bc04db5947849
Richard Pattinson <richie.pattinson@googlemail.com>
2021-11-25 14:36:32 +0000

    wip

--- app/services/backfill_site_uuid_by_code_per_year.rb
+++ app/services/backfill_site_uuid_by_code_per_year.rb
@@ -9,8 +9,7 @@ class BackfillSiteUuidByCodePerYear
     ActiveRecord::Base.transaction do
       sites = TeacherTrainingPublicAPI::Location.where(year: @recruitment_cycle_year, provider_code: @provider.code, course_code: @course.code).includes(:location_status).paginate(per_page: 500)
       sites.each do |site_from_api|
-        binding.pry
-        site_on_apply = Site.find_by(code: site_from_api.code, provider: @provider)
+        site_on_apply = Course.find_by(provider: @provider, code: @course.code, recruitment_cycle_year: @recruitment_cycle_year).sites.find_by(code: site_from_api.code)
         split_sites_and_assign_uuids_by_year(site_from_api, site_on_apply)
       end
     end
@@ -20,9 +19,11 @@ class BackfillSiteUuidByCodePerYear
     if @recruitment_cycle_year == RecruitmentCycle.current_year
       site_on_apply.update(uuid: site_from_api.uuid)
     else
-      duplicate_site = site_on_apply.dup.update(uuid: site_from_api.uuid)
-      site_on_apply.course_options.where(recruitment_cycle_year: site_from_api.year).update(site: duplicate_site)
-      duplicate_site.save!
+      duplicate_site = site_on_apply.dup
+      duplicate_site.update(uuid: site_from_api.uuid)
+      site_on_apply.course_options.joins(:course).where(courses: { recruitment_cycle_year: @recruitment_cycle_year }).each do |course_option|
+        course_option.update(site: duplicate_site)
+      end
     end
   end
 
--- spec/services/backfill_site_uuid_by_code_per_year_spec.rb
+++ spec/services/backfill_site_uuid_by_code_per_year_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe BackfillSiteUuidByCodePerYear do
   include TeacherTrainingPublicAPIHelper
   describe 'call' do
+    let!(:provider){ create(:provider)}
+    let!(:course){ create(:course, provider: provider)}
+    let!(:course_option) { create(:course_option, course: course, site: site_from_apply)}
+    let!(:site_from_apply){ create(:site, provider: provider, name: 'James Site', uuid: nil)}
     context 'when run for current year' do
-      let!(:provider){ create(:provider)}
-      let!(:course){ create(:course, provider: provider)}
-      let!(:course_option) { create(:course_option, course: course, site: site_from_apply)}
-      let!(:site_from_apply){ create(:site, provider: provider, name: 'James Site')}
       it 'puts uuids on sites in the current year' do
         uuid = SecureRandom.uuid
         stub_teacher_training_api_sites(provider_code: provider.code,
@@ -19,11 +19,9 @@ RSpec.describe BackfillSiteUuidByCodePerYear do
         expect(site_from_apply.reload.uuid).to eq(uuid)
       end
     end
+
     context 'when run for previous year' do
-      let!(:provider){ create(:provider)}
       let!(:course){ create(:course, provider: provider, recruitment_cycle_year: RecruitmentCycle.previous_year)}
-      let!(:course_option) { create(:course_option, course: course, site: site_from_apply)}
-      let!(:site_from_apply){ create(:site, provider: provider, name: 'James Site')}
       it 'puts duplicates sites from precious years and assigns uuids to them' do
         uuid = SecureRandom.uuid
         stub_teacher_training_api_sites(provider_code: provider.code,
@@ -31,9 +29,36 @@ RSpec.describe BackfillSiteUuidByCodePerYear do
                                         recruitment_cycle_year: RecruitmentCycle.previous_year,
                                         specified_attributes: [code: site_from_apply.code, uuid: uuid])
 
-        described_class.new(RecruitmentCycle.previous_year, provider, course).call
-        expect(Site.where(code: site_from_apply.code, provider: provider).count).to eq(2)
+        expect{ described_class.new(RecruitmentCycle.previous_year, provider, course).call }.to change(Site, :count).by(1)
         expect(Site.where(code: site_from_apply.code, provider: provider, uuid: uuid).count).to eq(1)
+      end
+    end
+    context 'when there is a course that has multiple sites across multiple years' do
+      let!(:course2){ create(:course, provider: provider, recruitment_cycle_year: RecruitmentCycle.previous_year)}
+      let!(:course_option2) { create(:course_option, course: course2, site: site_from_apply)}
+      it 'assigns the correct uuid' do
+        uuid = SecureRandom.uuid
+        uuid2 =SecureRandom.uuid
+        stub_teacher_training_api_sites(provider_code: provider.code,
+                                        course_code: course.code,
+                                        recruitment_cycle_year: RecruitmentCycle.current_year,
+                                        specified_attributes: [code: site_from_apply.code, uuid: uuid])
+
+        stub_teacher_training_api_sites(provider_code: provider.code,
+                                        course_code: course2.code,
+                                        recruitment_cycle_year: RecruitmentCycle.previous_year,
+                                        specified_attributes: [code: site_from_apply.code, uuid: uuid2])
+
+        expect{ described_class.new(RecruitmentCycle.previous_year, provider, course2).call }.to change(Site, :count).by(1)
+        expect(course_option2.reload.site.uuid).to eq(uuid2)
+        expect{ described_class.new(RecruitmentCycle.current_year, provider, course).call }.to change(Site, :count).by(0)
+        expect(course_option.site.uuid).to eq(uuid)
+      end
+    end
+
+    context 'where the site has been deleted from the API' do
+      it 'doesnt touch it' do
+        expect(1).to eq(1)
       end
     end
   end
